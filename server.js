eba50425beb934ba96ffd794d618efa8cf6c50f3
Elvedin Kaltak <74422974+kaltake00@users.noreply.github.com>
2022-09-12 09:56:47 +0200

    added docker

--- server.js
+++ server.js
@@ -10,13 +10,14 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }));
 
-
-
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, './.env') });
 
 const db = require('./models');
 const dbConfig = require('./config/db.config');
 const Products = db.products;
-db.mongoose.connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`,{
+db.mongoose.connect(process.env.MONGODB_URI,{
+    dbName: process.env.DB_NAME,
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(()=>{
@@ -32,7 +33,7 @@ require('./routes/user.routes')(app);
 require('./routes/product.routes')(app);
 
 app.get("/", (req,res)=>{
-    res.json({message: "Bismillah ir-rahman ir-rahim - U ime Allaha Mislostivog, Samilosnog!"})
+    res.json({message: "Welcome to the products list app"})
 })
 
 const PORT = process.env.PORT || 8080;
